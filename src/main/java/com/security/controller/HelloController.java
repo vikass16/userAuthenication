3cd63ee6bda23530ecf9d1b993653e60c945dc26
vikass16 <vk7199234@gmail.com>
2025-08-07 23:36:20 +0530

    Third commit

---
+++ src/main/java/com/security/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.security.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(HttpServletRequest request){
+        return "Welcome to Security "+request.getSession().getId();
+    }
+}
